907534def7c497825ce5a6d8bf6c84c719771342
hyching21 <sheep128763@gmail.com>
2025-01-24 00:57:01 +0800

    finish hw3

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
-from fastapi import FastAPI
-from pydantic import BaseModel 
+from fastapi import FastAPI, Query, Path
+from typing import Annotated
+from decimal import Decimal
+from pydantic import BaseModel
 
 app = FastAPI()
-# test test
-# http://localhost:8080/
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-# http://localhost:8080/items/{item_id}?q={q}
-@app.put("/items/{item_id}")
-async def read_item(item_id: int, q: str | None = None):
+#  --- HW3 ---
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id : Annotated[Decimal, Path(ge=1, le=1000, description="Item ID must be between 1 and 1000.")],
+    q : Annotated[str | None, Query(min_length=3, max_length=50, description = "Query 'q' must be between 3 and 50 characters.")] = None,
+    sort_order : Annotated[str, Query(pattern="^(asc|desc)$")] = "asc"
+):
     result = {
         "item_id": item_id,
-        "name": "Test Item",
-        "description": "A test description",
-        "price": 10.5,
-        "tax": 1.5
+        "description": f"This is a sample item." if not q else f"This is a sample item that matches the query {q}.",
+        "sort_order": sort_order
     }
+    
+    return result
+
+@app.put("/items/{item_id}")
+async def update_items(
+    item_id : Annotated[Decimal, Path(ge=1, le=1000, description="Item ID must be between 1 and 1000.")],
+    item: Item = None,
+    q : Annotated[str | None, Query(min_length=3, max_length=50, description = "Query 'q' must be between 3 and 50 characters.")] = None,
+):
+    result = {"item_id": item_id, **item.dict()}
+
     if q:
         result.update({"q": q})
+
     return result
